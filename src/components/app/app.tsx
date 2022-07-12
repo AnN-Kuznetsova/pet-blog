1b4e4af55b4ec770aa66fee74df71ac7fd3b0922
AnN <akimova_anna_25@mail.ru>
2022-07-12 12:30:18 +0300

    Добавит кнопку.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material";
 import React from "react";
 
 function App() {
   return (
     <div className="App">
-
+      <Button variant="contained">Button</Button>
     </div>
   );
 }
