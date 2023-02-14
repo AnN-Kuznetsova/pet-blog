7854a42ab39c7829b2427444a8849d1b47d2012f
AnN <akimova_anna_25@mail.ru>
2023-02-14 11:51:47 +0300

    Увеличение scrollbar при наведении на div.

--- src/components/app/styles.ts
+++ src/components/app/styles.ts
@@ -1,10 +1,8 @@
 import { Theme } from "@mui/material";
 
 
-const SCROLL_ACTIVE_SIZE = 30; // px
-const SCROLL_VISIBLE_SIZE = 8; // px
-
-const scrollRadius = SCROLL_VISIBLE_SIZE / 2;
+const SCROLL_ACTIVE_SIZE = 10; // px
+const SCROLL_VISIBLE_SIZE = 5; // px
 
 enum ScrollOrientation {
   VERTICAL,
@@ -16,46 +14,59 @@ enum GradientMode {
   RADIAL,
 }
 
-const GradientPosition = {
-  TO_RIGHT: `to right`,
-  TO_BOTTOM: `to bottom`,
-  TOP: `50% ${scrollRadius}px`,
-  BOTTOM: `50% calc(100% - ${scrollRadius}px)`,
-  LEFT: `${scrollRadius}px 50%`,
-  RIGHT: `calc(100% - ${scrollRadius}px) 50%`,
+const getGradientPosition = (orientation: ScrollOrientation, mode: GradientMode, size: number) => {
+  const radius = size / 2;
+
+  const position = {
+    TO_RIGHT: `to right`,
+    TO_BOTTOM: `to bottom`,
+    TOP: `50% ${radius}px`,
+    BOTTOM: `50% calc(100% - ${radius}px)`,
+    LEFT: `${radius}px 50%`,
+    RIGHT: `calc(100% - ${radius}px) 50%`,
+  };
+
+  if (mode === GradientMode.LINEAR) {
+    return orientation === ScrollOrientation.VERTICAL ?
+      [position.TO_BOTTOM] : [position.TO_RIGHT];
+  }
+
+  if (mode === GradientMode.RADIAL) {
+    return orientation === ScrollOrientation.VERTICAL ?
+      [position.TOP, position.BOTTOM]
+      : [position.LEFT, position.RIGHT];
+  }
+  return [];
 };
 
-const getGradients = (mode: GradientMode, positions: string[], color: string) => (
-  positions
+const getGradients = (mode: GradientMode, positions: string[], color: string, size: number) => {
+  const radius = size / 2;
+
+  return positions
     .map((position) => {
       if (mode === GradientMode.RADIAL) {
         return `radial-gradient(
           circle closest-side at ${position},
-          ${color} ${scrollRadius}px,
-          transparent ${scrollRadius}px)
+          ${color} ${radius}px,
+          transparent ${radius}px)
         `;
       }
 
       return `linear-gradient(
         ${position},
-        transparent ${scrollRadius}px,
-        ${color} ${scrollRadius}px,
-        ${color} calc(100% - ${scrollRadius}px),
-        transparent calc(100% - ${scrollRadius}px))
+        transparent ${radius}px,
+        ${color} ${radius}px,
+        ${color} calc(100% - ${radius}px),
+        transparent calc(100% - ${radius}px))
       `;
     })
-    .join()
-);
+    .join();
+};
 
-const getBackgroundStyles = (orientation: ScrollOrientation, color: string) => {
-  const borderStyle = `${SCROLL_ACTIVE_SIZE - SCROLL_VISIBLE_SIZE}px solid transparent`;
-
-  const radialGradientPositions = orientation === ScrollOrientation.VERTICAL ?
-    [GradientPosition.TOP, GradientPosition.BOTTOM]
-    : [GradientPosition.LEFT, GradientPosition.RIGHT];
-
-  const linearGradientPosition = orientation === ScrollOrientation.VERTICAL ?
-    [GradientPosition.TO_BOTTOM] : [GradientPosition.TO_RIGHT];
+const getBackgroundStyles = (orientation: ScrollOrientation, color: string, size: number) => {
+  const borderStyle = `${SCROLL_ACTIVE_SIZE - size}px solid transparent`;
+  const radialGradientPositions = getGradientPosition(orientation, GradientMode.RADIAL, size);
+  const linearGradientPosition = getGradientPosition(orientation, GradientMode.LINEAR, size);
 
   const borders = orientation === ScrollOrientation.VERTICAL ? {
     borderLeft: borderStyle,
@@ -65,15 +76,43 @@ const getBackgroundStyles = (orientation: ScrollOrientation, color: string) => {
 
   return {
     background: `
-    ${getGradients(GradientMode.LINEAR, linearGradientPosition, color)},
-    ${getGradients(GradientMode.RADIAL, radialGradientPositions, color)}`,
+      ${getGradients(GradientMode.RADIAL, radialGradientPositions, color, size)},
+      ${getGradients(GradientMode.LINEAR, linearGradientPosition, color, size)}`,
     backgroundClip: `padding-box`,
     ...borders,
   };
 };
 
 const globalStyles = (theme: Theme) => ({
-  "::-webkit-scrollbar": {
+  "*::-webkit-scrollbar": {
+    width: SCROLL_ACTIVE_SIZE,
+    height: SCROLL_ACTIVE_SIZE,
+
+    "&:vertical": getBackgroundStyles(ScrollOrientation.VERTICAL, theme.palette.primary.main, SCROLL_VISIBLE_SIZE),
+  },
+
+  "*::-webkit-scrollbar-thumb": {
+    "&:vertical": getBackgroundStyles(ScrollOrientation.VERTICAL, theme.palette.primary.light, SCROLL_VISIBLE_SIZE),
+  },
+
+  "div:hover": {
+    color: theme.palette.primary.light,
+
+    "&::-webkit-scrollbar": {
+      "&:vertical": getBackgroundStyles(ScrollOrientation.VERTICAL, theme.palette.primary.main, SCROLL_ACTIVE_SIZE),
+    },
+
+    "&::-webkit-scrollbar-thumb": {
+      "&:vertical": getBackgroundStyles(ScrollOrientation.VERTICAL, theme.palette.primary.light, SCROLL_ACTIVE_SIZE),
+    },
+  },
+
+  "div": {
+    // transition: `color .1s ease`,
+    transition: `all .1s ease`,
+  },
+
+  /* "::-webkit-scrollbar": {
     width: SCROLL_ACTIVE_SIZE,
     height: SCROLL_ACTIVE_SIZE,
 
@@ -88,7 +127,7 @@ const globalStyles = (theme: Theme) => ({
 
   "::-webkit-scrollbar-corner": {
     backgroundColor: `transparent`,
-  },
+  }, */
 
   /* "::-webkit-scrollbar-track": {
     background: getBackground(theme.palette.primary.main),
