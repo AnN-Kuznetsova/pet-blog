def5d04d9d4db5aab7fbb1ad6d2aa5f1980049e0
AnN <akimova_anna_25@mail.ru>
2023-03-06 11:52:19 +0300

    Изменит время анимации скрола.

--- src/components/app/styles.ts
+++ src/components/app/styles.ts
@@ -16,7 +16,7 @@ const ScrollParams = {
 };
 
 const ScrollAnimationParams = {
-  DURATION: 250, // ms
+  DURATION: 200, // ms
   FPS: 24,
   FRAMES_COUNT_SIZE_RELATION: 0.3, // подобрано опытным путём - компромисс между плавностью и количеством кадров
 };
