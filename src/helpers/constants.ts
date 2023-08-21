59f3076e976b327d983f3a64f8bde7913b07078f
AnN <akimova_anna_25@mail.ru>
2023-08-21 12:48:10 +0300

    Настроит конфигурацию на порту 3020 для имитации микрофронтов для Blog и Cookieclicker.

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -3,10 +3,12 @@ const SNACKBAR_AUTO_HIDE_DURATION = 5000; // ms
 const POST_LIST_ROW_HEIGHT = 86;
 const POST_TEXT_ROWS_COUNT = 5;
 
-enum AppRoute {
-  MAIN = `/`,
-  POST_PAGE = `/post/:id`,
-}
+const MAIN_ROOT = `/blog`;
+
+const AppRoute = {
+  MAIN: `${MAIN_ROOT}/`,
+  POST_PAGE: `${MAIN_ROOT}/post/:id`,
+};
 
 enum ModalType {
   NO_MODAL,
